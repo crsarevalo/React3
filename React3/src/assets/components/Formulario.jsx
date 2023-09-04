22d56e3b509e0554a87ce81b901b694069bcb69e
Cristian Arevalo <crsarevalo@gmail.com>
2023-09-04 00:48:27 -0300

    comentando el error

--- React3/src/assets/components/Formulario.jsx
+++ React3/src/assets/components/Formulario.jsx
@@ -21,53 +21,44 @@ const Formulario = ({ data, setData, setDataFilter, setAlert }) => {
     const inputProp = inputs[e.target.id];
     if (inputProp) {
       setDatosColab({ ...datosColab, [inputProp]: e.target.value });
-    } 
+    }
   };
 
   const validardatos = (e) => {
-e.preventDefault() 
+    e.preventDefault();
 
+    const { nombre, correo, edad, cargo, telefono } = datosColab;
 
-const {nombre, correo, edad, cargo, telefono} = datosColab
+    const validarValores = !nombre || !correo || !edad || !cargo || !telefono;
 
+    validarValores
+      ? setAlert({
+          error: true,
+          mensaje: "completa los campos",
+          color: "text-danger",
+        })
+      : setAlert({
+          error: false,
+          mensaje: "cuenta creada con exito",
+          color: "text-success",
+        });
 
-const validarValores = !nombre || !correo || !edad || !cargo || !telefono
+    setDatosColab({
+      nombre: "",
+      correo: "",
+      edad: "",
+      cargo: "",
+      telefono: "",
+    });
+  };
 
-
-validarValores ? 
-setAlert({
-    error: true,
-    mensaje: "completa los campos",
-    color: "text-danger",
-  })
-  
-: setAlert({
-    error: false,
-    mensaje: "cuenta creada con exito",
-    color: "text-success",
-  });
-
-  setDatosColab({
-    nombre: "",
-    correo: "",
-    edad: "",
-    cargo: "",
-    telefono: "", 
-  });
-
-  }
-
-    const Nuevotrabajador = { ...datosColab, id: data.length + 1 };
-    setData([...data, Nuevotrabajador]);
-    setDataFilter([...data, Nuevotrabajador]); 
-
-
+  //const Nuevotrabajador = { ...datosColab, id: data.length + 1 };
+  //setData([...data, Nuevotrabajador]);
+  //setDataFilter([...data, Nuevotrabajador]);
 
   return (
     <div className="formulario text-center bg-dark p-5 rounded">
-      <form noValidate
-      onSubmit={validardatos} 
-      >
+      <form noValidate onSubmit={validardatos}>
         <div className="mb-2">
           <input
             type="text"
